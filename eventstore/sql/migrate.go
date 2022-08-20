014c4db53baa5653da85fe4b9369e461b8c5286b
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2022-08-20 13:14:33 -0400

    id → aggregate_id (#6)

--- eventstore/sql/migrate.go
+++ eventstore/sql/migrate.go
@@ -2,14 +2,14 @@ package sql
 
 import "context"
 
-const createTable = `CREATE TABLE events (seq INTEGER PRIMARY KEY AUTOINCREMENT, id UUID NOT NULL, version INTEGER, reason VARCHAR, type VARCHAR, timestamp VARCHAR, data BLOB, metadata BLOB);`
+const createTable = `CREATE TABLE events (seq INTEGER PRIMARY KEY AUTOINCREMENT, aggregate_id UUID NOT NULL, version INTEGER, reason VARCHAR, type VARCHAR, timestamp VARCHAR, data BLOB, metadata BLOB);`
 
 // Migrate the database
 func (s *SQL) Migrate() error {
 	sqlStmt := []string{
 		createTable,
-		`CREATE UNIQUE INDEX id_type_version ON events(id, type, version);`,
-		`CREATE INDEX id_type ON events (id, type);`,
+		`CREATE UNIQUE INDEX id_type_version ON events(aggregate_id, type, version);`,
+		`CREATE INDEX id_type ON events (aggregate_id, type);`,
 	}
 	return s.migrate(sqlStmt)
 }
